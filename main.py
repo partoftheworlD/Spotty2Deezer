c1325e1982d8b81e1459181c73d74633efd2f7c5
partoftheworlD <partoftheworldcs@gmail.com>
2021-08-26 11:49:27 +0300

    7387677

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import json
 import requests
-import re
-import socket
 
 class Spotify:
-    def __init__(self, playlist_id) -> None:
+    def __init__(self, token, playlist_id) -> None:
         self.track_list = []
-        self.token = ''
-        self.client_id = ''
-        self.header = {}
+        self.token = token
+        self.header = { 'Accept': 'application/json',
+                        'Authorization': f'Bearer {self.token}'}
         self.playlist_api = 'https://api.spotify.com/v1/playlists/'
         self.playlist_id = playlist_id
         self.playlist_name = ''
@@ -22,19 +20,6 @@ class Spotify:
             self.playlist_name = self.body['name']
         else:
             print('[+] ', response.status_code)
-
-    def getToken(self):
-        rc = re.compile(r'(\=[A-Za-z0-9\-\_]+)).*')
-        # TODO: Doesn't work like a Deezer version
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serversocket.bind(('', 5000))
-        serversocket.listen(5)
-        requests.get(f'https://accounts.spotify.com/authorize?response_type=token&client_id={self.client_id}\
-            &scope=playlist-read-private&redirect_uri=http://localhost:5000/callback')
-        (clientsocket, _) = serversocket.accept()
-        self.token = re.findall(rc, str(clientsocket.recv(512)))
-        serversocket.close()
-        self.header = {'Accept': 'application/json','Authorization': f'Bearer {self.token}'}
     
     def getPlaylist(self):
         self.getPlaylistInfo()
@@ -63,48 +48,18 @@ class Spotify:
         
 
 class Deezer:
-    def __init__(self, playlist_name, track_list, app_id, secret) -> None:
-        self.app_id = app_id
-        self.secret = secret
-        self.access_token = ''
+    def __init__(self, playlist_name, track_list, access_token, user_id) -> None:
+        self.access_token = access_token
         self.playlist_name = playlist_name
-        self.user_id = ''
+        self.user_id = user_id
         self.playlist_id = ''
         self.track_list = track_list
-        self.code = ''
 
     def createPlaylist(self):
         response = requests.post(
             f'https://api.deezer.com/user/{self.user_id}/playlists/?title={self.playlist_name}&request_method=post&access_token={self.access_token}')
         if response.status_code == requests.status_codes.codes.ok:        
             self.playlist_id = response.json()['id']
-
-    def getCode(self):
-        rc = re.compile('(fr[a-z0-9]+).*')
-
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serversocket.bind(('', 5000))
-        serversocket.listen(5)
-        requests.get(
-            f'https://connect.deezer.com/oauth/auth.php?app_id={self.app_id}&\
-            redirect_uri=http://localhost:5000/callback&perms=manage_library')
-        (clientsocket, _) = serversocket.accept()
-        self.code = re.findall(rc, str(clientsocket.recv(64)))[0]
-        serversocket.close()
-
-    def getToken(self):
-        self.getCode()
-        rc = re.compile('(fr[0-9a-zA-Z]+).*')
-        data = requests.get(
-            f'https://connect.deezer.com/oauth/access_token.php?app_id={self.app_id}\
-                &secret={self.secret}&code={self.code}').text
-        self.access_token = re.findall(rc, data)[0]
-        self.getUserID()
-
-    
-    def getUserID(self):
-        self.user_id = requests.get(f'https://api.deezer.com/user/me?access_token={self.access_token}').json()['id']
-
 
     def addTracks(self):
         for query in [track for idx in range(len(self.track_list)) for track in self.track_list[idx]]:
@@ -124,10 +79,9 @@ if __name__ == '__main__':
     spotify.getPlaylist()
     deezer = Deezer(spotify.playlist_name,
                     spotify.track_list,
-                    '<APP-ID>',
-                    '<ACCESS-TOKEN>')
+                    '<ACCESS-TOKEN>',
+                    '<USER-ID>')
 
-    deezer.getToken()
     deezer.createPlaylist()
     deezer.addTracks()
     
